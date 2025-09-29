40ab3165d61bfe5b4429a206bc28d6a92f092bec
rahma <gamalrahma65@gmail.com>
2025-09-29 17:04:30 +0300

    New function

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 
 let h1= document.getElementById("h1")
-
+let btn = document.getElementById("btn")
 function change(){
     h1.style.backgroundColor="red"
 }
+btn.addEventListener("click",function(){
+
+h1.style.backgroundColor="green"
+document.body.style.backgroundColor="black"
+
+})
